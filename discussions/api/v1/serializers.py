7fad92cc39db4c0c8dec568a1876041b5f637051
Daniel Friedman <dfriedman58@gmail.com>
2015-07-24 16:33:11 +0000

    WIP implement threads endpoint

--- discussions/api/v1/serializers.py
+++ discussions/api/v1/serializers.py
@@ -1,5 +1,6 @@
-from discussions.models import User
-from rest_framework_mongoengine.serializers import EmbeddedDocumentSerializer
+from discussions.models import CommentThread, Content, User
+from rest_framework_mongoengine.serializers import DocumentSerializer, EmbeddedDocumentSerializer
+from rest_framework.pagination import PaginationSerializer
 
 
 class ReadStatesSerializer(EmbeddedDocumentSerializer):
@@ -7,8 +8,25 @@ class ReadStatesSerializer(EmbeddedDocumentSerializer):
         model = User.ReadState
 
 
-class UserSerializer(EmbeddedDocumentSerializer):
+class UserSerializer(DocumentSerializer):
     read_states = ReadStatesSerializer(many=True)
 
     class Meta:
         model = User
+
+
+class VoteSerializer(EmbeddedDocumentSerializer):
+    class Meta:
+        model = Content.Votes
+
+
+class ThreadSerializer(DocumentSerializer):
+    votes = VoteSerializer()
+
+    class Meta:
+        model = CommentThread
+
+
+class PaginatedThreadSerializer(PaginationSerializer):
+    class Meta(object):
+        object_serializer_class = ThreadSerializer
--- discussions/api/v1/views.py
+++ discussions/api/v1/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
-from discussions.models import User
-from discussions.api.v1.serializers import UserSerializer
+from discussions.models import CommentThread, User
+from discussions.api.v1.serializers import PaginatedThreadSerializer, UserSerializer
 
 
 class UserDetailView(generics.RetrieveAPIView):
@@ -15,3 +16,18 @@ class UserDetailView(generics.RetrieveAPIView):
     # shouldn't be necessary in theory but DRF is returning Not Found otherwise
     def get_object(self):
         return User.objects.get(external_id=self.kwargs['external_id'])
+
+
+class ThreadListView(generics.ListAPIView):
+    """
+    API endpoint that allows threads to be viewed.
+    """
+    paginate_by = 10
+    paginate_by_param = 'per_page'
+    pagination_serializer_class = PaginatedThreadSerializer
+
+    def get(self, request):
+        comment_threads = CommentThread.objects
+        page = self.paginate_queryset(comment_threads)
+        serializer = self.pagination_serializer_class(page)
+        return Response(serializer.data)
